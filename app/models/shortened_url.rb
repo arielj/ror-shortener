8b1d3f9782fcbaa67c051515ef0b8fc780a03ba8
Ariel Juodziukynas <arieljuod@gmail.com>
2019-04-03 21:40:21 -0300

    Methods to encode/decode the id to/from base62

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,4 +1,6 @@
 class ShortenedUrl < ApplicationRecord
+  BASE_62_DIGITS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
   validates :original_url, presence: true, uniqueness: true
 
   scope :top, -> { order(views_counter: :desc).limit(100) }
@@ -12,13 +14,33 @@ class ShortenedUrl < ApplicationRecord
   end
 
 
-  # TODO: decode if from base62
   def self.decode_key(encoded)
-    encoded
+    base62_to_num(encoded)
   end
 
-  # TODO: encode id to base62
   def encoded_key
-    id
+    self.class.num_to_base62_padded(id)
+  end
+
+private
+  def self.num_to_base62(num)
+    return '' if num.nil?
+
+    _next = (num / 62).floor unless num < 62
+
+    "#{num_to_base62(_next)}#{BASE_62_DIGITS[num % 62]}"
+  end
+
+  def self.num_to_base62_padded(num, pad = 7)
+    raise 'Invalid padding' if num > (62**7-1)
+
+    encoded = num_to_base62(num)
+    "0"*(7-encoded.length)+encoded
+  end
+
+  def self.base62_to_num(encoded)
+    encoded.reverse.each_char.with_index.map do |c, idx|
+      BASE_62_DIGITS.index(c) * (62**idx)
+    end.sum
   end
 end
