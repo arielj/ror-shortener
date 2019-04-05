79ac084da034fd581fdef73cfe04fb5ae71eaad3
Ariel Juodziukynas <arieljuod@gmail.com>
2019-04-05 01:44:16 -0300

    change redirect url on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def root
-    redirect_to 'http://localhost:4200'
+    if Rails.env.production?
+      redirect_to 'https://angular-shortener-client.herokuapp.com/shortener'
+    else
+      redirect_to 'http://localhost:4200'
+    end
   end
 end
